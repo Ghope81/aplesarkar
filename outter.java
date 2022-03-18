ef621ff4ea9846f83e103227effae16fc4d88e66
Ghope81 <101719579+Ghope81@users.noreply.github.com>
2022-03-18 09:54:35 +0530

    Add files via upload

---
+++ outter.java
@@ -0,0 +1,16 @@
+package NestedDemo;
+
+public class outter {
+	
+	void method() {
+		System.out.println("this is outter class method");
+	}
+	
+	 class inner{
+		
+	void test() {
+		System.out.println("this is inner class method");
+	}
+	}
+
+}
